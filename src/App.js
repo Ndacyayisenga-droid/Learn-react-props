1ab129445349e33385127c95896f36381a026b2d
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2022-08-03 01:03:03 +0300

    Learn react props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Jokes from './jokes';
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Jokes 
+          setup ="Jerry Laruba Festus"
+          punchline ="Artist"
+      />
+      <Jokes
+          setup ="Noah Ndacyayisenga"
+          punchline ="Coder"
+      />
+      <Jokes 
+          setup ="Lumu Christopher"
+          punchline ="Business guy"
+      />
+      <Jokes 
+          setup ="Nsereko Josh"
+          punchline ="Joker"
+      />
+      <Jokes 
+          setup ="Agaba Derrick"
+          punchline ="Short man"
+      />
     </div>
   );
 }
